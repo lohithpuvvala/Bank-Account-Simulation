6f9a0a66cee8ee89bb521f593b79f5257dd9abe5
lohithpuvvala <lohithpuvvala@gmail.com>
2025-06-30 11:51:03 +0530

    Update Main. Create Basic Menu Interface

--- src/com/lohithpuvvala/Main.java
+++ src/com/lohithpuvvala/Main.java
@@ -1,7 +1,38 @@
 package com.lohithpuvvala;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main");
+        Scanner in = new Scanner(System.in);
+        SavingsAccount savingsAccount = new SavingsAccount("Lohit", "SB1001", 5000);
+
+        while(true){
+            System.out.println("\n====== Bank Menu ======");
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Print Transactions History");
+
+            int choice = in.nextInt();
+
+            switch (choice){
+                case 1: //Deposit
+                    break;
+                case 2: //Withdraw
+                    break;
+                case 3://check balance
+                    break;
+                case 4://Print Transaction History
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+
+            System.out.println("Do you want to continue? (y/n)");
+            if(in.next().equalsIgnoreCase("n")){
+                break;
+            }
+        }
     }
 }
