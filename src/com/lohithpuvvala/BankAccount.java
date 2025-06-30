33e06c117e7676f529d9938529dc85f473c96c09
lohithpuvvala <lohithpuvvala@gmail.com>
2025-06-30 11:40:51 +0530

    Update BankAccount. Create deposit, withdraw, getBalance & printTransactionsHistory methods

--- src/com/lohithpuvvala/BankAccount.java
+++ src/com/lohithpuvvala/BankAccount.java
@@ -17,4 +17,38 @@ public class BankAccount {
 
         transactionsHistory.add("Account created with balance: ₹"+ initialBalance);
     }
+
+    public void deposit(double amount)
+    {
+        if(amount > 0)
+        {
+            balance += amount;
+            transactionsHistory.add("Desposited ₹"+amount+" | Balance: ₹"+balance);
+        }else{
+            transactionsHistory.add("Failed to deposit amount of ₹"+amount);
+        }
+    }
+
+    public void withdraw(double amount){
+        if(amount > 0 && amount <= balance){
+            balance -= amount;
+            transactionsHistory.add("Withdraw ₹"+amount+" |Balance: ₹"+balance);
+        }else if(amount > balance){
+            System.out.println("Insufficient balance");
+        }
+        else{
+            transactionsHistory.add("Failed withdrawal attempt of ₹"+amount);
+        }
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public void printTransactionsHistory(){
+        System.out.println("Transactions History for Account: "+accountNumber);
+        for(String t: transactionsHistory){
+            System.out.println(t);
+        }
+    }
 }
